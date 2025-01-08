45f5ca852faec4dac1b8c02dca166e31325fab18
ronitpandya <ronitpandya@gmail.com>
2025-01-08 23:56:18 +0530

    Day 1 progress

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,4 @@
-# Write your code below this line 👇
+print("Hello buddy, let's generate your band name")
+city_name = input("Hi, what's the name of your city? \n")
+pet_name = input("And how about name of your pet? \n")
+print("Your band name is " + city_name + " " + pet_name)
