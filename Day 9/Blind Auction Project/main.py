9c0bfb6b0f9fa625efbf61909eccebcacbfcd2fd
ronitpandya <ronitpandya@gmail.com>
2025-01-21 08:57:56 +0530

    Day 5 progress

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,39 @@
 # TODO-1: Ask the user for input
+import art
+auction = {}
+auctionlive = True
+print(art.logo)
+# auction[input("Please write your name")] = input("give your buying price")
+
+def find_highest_bidder(auction):
+    winner = ""
+    highest_bid = 0
+    for bidder in auction:
+        if auction[bidder] > highest_bid:
+            highest_bid = auction[bidder]
+            winner = bidder
+    print(f"Highest bidder is {winner} bidding amount is ${highest_bid}")
+
+
+
+
+while auctionlive:
+    user_name = input("Please write your name")
+    price = int(input("give your buying price"))
+    auction[user_name] = price
+    yesorno = input("is there another bid? type yes or no").lower()
+    if yesorno == "no":
+        find_highest_bidder(auction)
+        auctionlive = False
+    else:
+        print("\n" * 100)
+
+
+
+
+
+
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
-
 
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,25 @@
 programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+
+student_grades = {}
+#logic for scores to grades
+
+for name, value in student_scores.items():
+    if value < 70 :
+        student_grades[name] = "Fail"
+    elif value < 81:
+        student_grades[name] = "Acceptable"
+    elif value < 91:
+        student_grades[name] = "Exceeds Expectations"
+    else:
+        student_grades[name] = "Outstanding"
+
+print(student_grades)
